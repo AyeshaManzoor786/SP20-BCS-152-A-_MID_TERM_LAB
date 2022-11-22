ae0812f07370463f5650503713c8814f9ef5f04e
AyeshaManzoor786 <SP20-BCS-152@CUILAHORE.EDU.PK>
2022-11-22 16:35:19 +0500

    mid term paper

--- myJava.js
+++ myJava.js
@@ -1,5 +1,9 @@
 console.log("File is Attach here!!");
 $(function(){
     console.log("Start Binding");
-  
+   $(".img2").click(handleFunction);
 });
+
+function handleFunction(){
+    alert("You click sign in!!");
+}
